27bcf28e37b7e604780b0d34ee5c5ebbaf400da8
Theranda Jashari <therandajashari@Therandas-MacBook-Pro.local>
2023-07-29 15:52:44 -0400

    Finished up section 1-2

--- section_1/data_preparation.R
+++ section_1/data_preparation.R
@@ -161,10 +161,10 @@ fin[!complete.cases(fin),]
 
 fin[!complete.cases(fin),]
 
-median(fin[,"Employees"], na.rm = T)
+median(fin[,"Employees"], na.rm = T) #getting the median of all employees regardless of industry 
 mean(fin[,"Employees"], na.rm = T)
 
-median(fin[fin$Industry=="Retail","Employees"], na.rm = T) 
+median(fin[fin$Industry=="Retail","Employees"], na.rm = T) #getting the median if employees in retail industry
 med_empl_retail <- median(fin[fin$Industry=="Retail","Employees"], na.rm = T) 
 med_empl_retail 
 
@@ -174,7 +174,7 @@ fin[is.na(fin$Employees) & fin$Industry == "Retail", "Employees"] <- med_empl_re
 fin[3,] 
 
 #fill the Employees for Financial Service 
-med_empl_finserv <- median(fin[fin$Industry=="Financial Services","Employees"], na.rm = T) 
+med_empl_finserv <- median(fin[fin$Industry=="Financial Services","Employees"], na.rm = T) #removing na when calculating median
 
 fin[is.na(fin$Employees) & fin$Industry == "Financial Services",] 
 fin[is.na(fin$Employees) & fin$Industry == "Financial Services", "Employees"] <- med_empl_finserv
@@ -184,4 +184,86 @@ fin[330,]
 fin[!complete.cases(fin),]
 
 #------------------Replacing missing data: Median Imputation Method (Part 2) --------------------
+
+fin[!complete.cases(fin),]
+med_growth_constr <- median(fin[fin$Industry=="Construction","Growth"], na.rm = T) 
+fin[is.na(fin$Growth) & fin$Industry == "Construction", "Growth"] <- med_growth_constr
+#check
+fin[8,]
+
+#------------------Replacing missing data: Median Imputation Method (Part 3) --------------------
+fin[!complete.cases(fin),]
+
+#Revenue 
+med_revenue_constr <- median(fin[fin$Industry=="Construction", "Revenue"], na.rm =T)
+med_revenue_constr
+
+fin[is.na(fin$Revenue) & fin$Industry=="Construction","Revenue"] <- med_revenue_constr
+fin[c(8,42),]
+
+fin[!complete.cases(fin),]
+
+#expenses
+#we don't want to replace expenses for row 15 (thats by itself) because the row wont add up 
+med_exp_constr <- median(fin[fin$Industry==c("Construction"), "Expenses"], na.rm =T)
+med_exp_constr
+
+fin[is.na(fin$Expenses) &fin$Industry == "Construction", "Expenses"] <- med_exp_constr
+fin[c(8,15,42),]
+
+#what if IT services was in the construction industry but we wouldn't want to pick that? 
+#add another filter layer (that profit also has to be missing)
+fin[is.na(fin$Expenses) & fin$Industry == "Construction" & is.na(fin$Profit)]
+fin[is.na(fin$Expenses) & fin$Industry == "Construction" & is.na(fin$Profit), "Expenses"] <- med_exp_constr
+fin[!complete.cases(fin),]
+#what if we want to change both of them at the same time (in different categories)? 
+
+
+#------------------Replacing missing data: Deriving values method --------------------
+
+#Revenue - Expenses = Profit 
+#Expenses = Revenue - Profit 
+
+fin[is.na(fin$Profit), "Profit"] <- fin[is.na(fin$Profit), "Revenue"] - fin[is.na(fin$Profit), "Expenses"]
+fin[c(8,42),]
+
+fin[is.na(fin$Expenses), "Expenses"] <- fin[is.na(fin$Expenses), "Revenue"] - fin[is.na(fin$Expenses), "Profit"]
+fin[15,]
+
+fin[!complete.cases(fin),]
+#--------------------------Visualizing results ------------------------------
+library(ggplot2)
+
+#A scatterplot classified by industry, showing revenue, expenses, profit 
+
+p <- ggplot(data=fin)
+p + geom_point(aes(x=Revenue , y=Expenses, 
+                   color=Industry, size = Profit))
+
+#A scatterplot that includes industry trends for the expenses-revenue relationship 
+ #preset aesthetics 
+d <- ggplot(data=fin,aes(x=Revenue , y=Expenses, 
+                          color=Industry)) 
+
+d + geom_point() + 
+  geom_smooth(fill=NA, size=1.2)
+
+#Boxplot showing growth by Industry 
+
+f <- ggplot(data=fin, aes(x=Industry, y=Growth, color=Industry))
+f + geom_boxplot(size=1)
+
+f + geom_jitter() +
+    geom_boxplot(size=1, alpha=0.5, outlier.color =NA)
+
+
+
+
+
+
+
+
+
+
+
 
