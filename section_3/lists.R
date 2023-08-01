2a41abd861639ba488e20ce46bc528f8e04bf1b3
Theranda Jashari <therandajashari@Therandas-MBP.lan>
2023-07-31 21:49:19 -0400

    updates

---
+++ section_3/lists.R
@@ -0,0 +1,13 @@
+#Lists in R
+
+#Deliverable - a list with the following components:
+#Character:   Machine Name 
+#Vector:      (min, mean, max) Utilization for the month(excluding missing hours)
+#Logical:     Has utilization ever fallen below 90%? TRUE/FALSE
+#Vector:      All hours where utilization is unknown (NAs)
+#Dataframe:   For this machine 
+#Plot:        For all machines 
+
+getwd()
+setwd("/Users/therandajashari/Documents/r_udemy_classes_main/07_23_r_data_class/section_3")
+
